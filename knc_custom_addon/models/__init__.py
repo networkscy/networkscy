b868791bbbf36507cca60575572ef8191dd1793a
networkscy <info@networkscy.com>
2022-07-08 16:06:07 +0300

    Update __init__.py

--- knc_custom_addon/models/__init__.py
+++ knc_custom_addon/models/__init__.py
@@ -2,6 +2,4 @@ from . import partner
 from . import partner_names
 from . import partner_profiles
 from . import partner_relations
-from . import partner_street_names
-from . import partner_tweak_addresses
 # from . import x_product_attribute_value_supplier
